d992d3d475deb9c56c904fd1593a4c92ef487a08
Sujal Paghdar <sujalpaghdar200805@gmail.com>
2025-03-24 11:14:40 +0530

    demo

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
         element: <MainPage />,
       },
       {
-        path: "/quiz-builder",
+        path: " ",
         element: <BuildQuiz />,
       },
     ],
